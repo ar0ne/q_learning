d840803125660a256c91b634347f722ed6162c7d
ar1 <ja_he@mail.ru>
2017-11-22 01:50:22 +0300

    improvements

--- single_agent/main.py
+++ single_agent/main.py
@@ -21,41 +21,56 @@ class State():
     def move_bottom(self):
         return State(self.pos_x + 1, self.pos_y)
 
+    def __hash__(self):
+        return hash((self.pos_x, self.pos_y))
+
+    def __eq__(self, other):
+        return (self.pos_x, self.pos_y) == (other.pos_x, other.pos_y)
+
+    def __ne__(self, other):
+        return not(self == other)
+
+    def __str__(self):
+        return "(%d, %d)" % (self.pos_x, self.pos_y)
+
 
 class QLearning():
     def __init__(self):
         self.WIDTH = 16
         self.HEIGHT = 9
 
-        self.Q = [[0 for x in range(self.WIDTH)] for y in range(self.HEIGHT)]
+        self.Q = self.init_q()
 
         # Rewards matrix
-        # self.R = [
-        #     [-1, -1, -1, -1, -1, -1, -100, -1, -1, -1, -1, -1, -1, -1, -1, -1],
-        #     [-1, -1, -1, -1, -1, -1, -100, -1, -1, -1, -1, -1, -1, -1, -1, -1],
-        #     [-1, -1, -1, -1, -1, -1, -100, -1, -1, -1, -1, -1, -1, -100, -1, -1],
-        #     [-1, -1, -1, -1, -1, -1, -100, -1, -1, -1, -1, -1, -1, -100, -1, -1],
-        #     [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -100, -1, -1],
-        #     [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -100, -1, -1],
-        #     [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -100, -1, -1],
-        #     [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -100, -1, -1],
-        #     [-1, -1, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -1, 100]
-        # ]
         self.R = [
-            [0, 0, 0, 0, 0, 0, -100, 0, 0, 0, 0, 0, 0, 0, 0, -1],
-            [0, 0, 0, 0, 0, 0, -100, 0, 0, 0, 0, 0, 0, 0, 0, -1],
-            [0, 0, 0, 0, 0, 0, -100, 0, 0, 0, 0, 0, 0, -100, 0, -1],
-            [0, 0, 0, 0, 0, 0, -100, 0, 0, 0, 0, 0, 0, -100, 0, -1],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -100, 0, -1],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -100, 0, -1],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -100, 0, -1],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -100, 0, -1],
+            [0, 0, 0, 0, 0, 0, -100, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, -100, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, -100, 0, 0, 0, 0, 0, 0, -100, 0, 0],
+            [0, 0, 0, 0, 0, 0, -100, 0, 0, 0, 0, 0, 0, -100, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -100, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -100, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -100, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -100, 0, 0],
             [0, 0, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, -100, 0, 100]
         ]
 
+        self.score = 10
+        self.walk_reward = -0.04
 
         self.Gamma = .8
         self.Epochs = 10
+
+    def init_q(self):
+        q = {}
+        for x in xrange(self.WIDTH):
+            for y in xrange(self.HEIGHT):
+                state = State(x, y)
+                actions = self.get_allowed_actions(state)
+                temp = {}
+                for action in actions:
+                    temp[action()] = 0.1
+                q[state] = temp
+        return q
 
     def is_moveable_to_the_left(self, state):
         return state.pos_y > 0
@@ -82,44 +97,52 @@ class QLearning():
         return allowed
 
     def choose_next_action(self, state):
-        allowed = self.get_allowed_actions(state)
-        return random.choice(allowed)()
+        allowed = self.Q[state].items()
+        return random.choice(allowed)[0]
 
     def calculate_q(self, state, action):
         # Q(state, action) = R(state, action) + Gamma * Max[Q(next state, all actions)]
         return self.R[state.pos_y][state.pos_x] + self.Gamma * self.get_max_q(action)
 
     def get_max_q(self, next_state):
-        allowed = self.get_allowed_actions(next_state)
-        max = 0
-        for action in allowed:
-            allowed_state = action()
-            if self.Q[allowed_state.pos_y][allowed_state.pos_x] > max:
-                max = self.Q[allowed_state.pos_y][allowed_state.pos_x]
-        return max
+        allowed = self.Q[next_state].items()
+        max_v = -1.
+        for allowed_state in allowed:
+            if allowed_state[1] > max_v:
+                max_v = allowed_state[1]
+        return max_v
 
     def default_state(self):
         return State(0, 8)
 
+    def update_q(self, state, action, alpha, increment):
+        self.Q[state][action] *= 1 - alpha
+        self.Q[state][action] += alpha * increment
+
     def training(self):
         # init state
         state = self.default_state()
-        count = 0
-
+        count = 1
+        alpha = 1
+        tick = 1
         while count < self.Epochs:
 
             # print("%d : %d" % (state.pos_x, state.pos_y))
 
             action = self.choose_next_action(state)
 
-            self.Q[state.pos_y][state.pos_x] = self.calculate_q(state, action)
+            max_q = self.calculate_q(state, action)
 
-            self.show_progress(self.Q, state)
+            self.score -= self.walk_reward
 
-            if self.R[action.pos_y][action.pos_x] == -100:
+            self.update_q(state, action, alpha, max_q + self.score)
+
+            self.show_progress(state, action)
+
+            if self.is_game_failed(action):
                 # print("Failed")
                 state = self.default_state()
-            elif action.pos_x == 15 and action.pos_y == 8:
+            elif self.is_game_won(action):
                 # goal completed, start next epoch
                 count += 1
                 state = self.default_state()
@@ -127,18 +150,26 @@ class QLearning():
             else:
                 state = action
 
+            # Update the learning rate
+            alpha = pow(tick, -0.1)
+
+    def is_game_failed(self, action):
+        return self.R[action.pos_y][action.pos_x] == -100
+
+    def is_game_won(self, action):
+        return action.pos_x == 15 and action.pos_y == 8
+
     def normalize_q(self):
         pass
 
-    @staticmethod
-    def show_progress(q_matrix, new_action):
+    def show_progress(self, state, new_action):
         os.system('cls' if os.name == 'nt' else 'clear')
-        for rdx, row in enumerate(q_matrix):
-            for cdx, val in enumerate(row):
-                if new_action.pos_x == cdx and new_action.pos_y == rdx:
-                    print('\033[0;31;40m%6.2f' % val, end='')
+        for i in xrange(self.HEIGHT):
+            for j in xrange(self.WIDTH):
+                if new_action.pos_x == j and new_action.pos_y == i:
+                    print('\033[0;31;40m%6.2f' % self.Q[state][new_action], end='')
                 else:
-                    print('\033[1;32;40m%6.2f' % val, end='')
+                    print('\033[1;32;40m%6d' % self.R[i][j], end='')
             print()
         print()
         time.sleep(0.1)
